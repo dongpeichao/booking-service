f3e923a7c5acf16a60621d583c807d0675f52441
peichao.dong <peichao.dong@thoughtuworks.com>
2020-10-23 13:27:57 +0800

    chore: format code

--- src/test/java/cn/dpc/booking/repository/OrderRepositoryTest.java
+++ src/test/java/cn/dpc/booking/repository/OrderRepositoryTest.java
@@ -1,7 +1,6 @@
 package cn.dpc.booking.repository;
 
 import cn.dpc.booking.domain.BookingOrder;
-import cn.dpc.booking.domain.OrderType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
